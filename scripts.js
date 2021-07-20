e5a2f1d7bc76ad7d8684907f03243819252b020d
kite <sarang.thekkedathpr@gmail.com>
2021-07-20 22:45:39 +0530

    initial commit

---
+++ scripts.js
@@ -0,0 +1,19 @@
+const startingMinutes = 1;
+let time = startingMinutes * 60;
+
+const countdownTimer = document.getElementById('countdown');
+
+setInterval(updateCountDown , 1000)
+
+function updateCountDown(){
+    if (time >= 0){
+        const minutes  = Math.floor(time/60);
+        let seconds = time % 60
+        countdownTimer.innerHTML = `${minutes} : ${seconds}`;
+        console.log(`${minutes} : ${seconds}`)
+        time --;
+    }else{
+        console.log('over')
+        window.location.replace('/completed.html')
+    }
+}
